5c9750368de044ffad0bd13eecdcc141d57e0344
P3-J <jako.keskkyla@gmail.com>
2024-10-11 18:58:44 +0300

    controller for gravity

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,24 +1,40 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 5.5
+var speed = 5.0
+var jump_speed = 10.0  
+var gravity = -30.0  
+var max_fall_speed = -60.0  
 
 
 func _physics_process(delta: float) -> void:
+
+	var direction := Vector3()
+
+	if Input.is_action_pressed("up"):
+		direction.z -= 1
+	if Input.is_action_pressed("down"):
+		direction.z += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+
+	direction = direction.normalized()
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.y += gravity * delta
+		# clamp velocity independent of gravity
+		if velocity.y < max_fall_speed:
+			velocity.y = max_fall_speed
 
 	move_and_slide()
+
+	if is_on_floor() and velocity.y < 0:
+		velocity.y = 0
+
