da580c20089beece8c330196ac7ecdba742c95e0
Stremper <70448635+Stremper@users.noreply.github.com>
2024-10-11 20:41:07 +0300

    UI + global signalbus

---
+++ src/scripts/pausemenu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var main_menu_scene =  ResourceLoader.load("res://src/scenes/main_menu.tscn") as PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		if visible:
+			hide()
+			get_tree().paused = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			show()
+			get_tree().paused = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().change_scene_to_packed(main_menu_scene)
+
+
+func _on_respawn_button_pressed() -> void:
+	Signalbus.respawn_called.emit()
--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -7,46 +7,50 @@ var max_fall_speed = -60.0
 
 var mouse_sensitivity = 0.01
 @onready var head: Node3D
-var y_rotation = 0.0  
+var y_rotation = 0.0
+@onready var pause_menu = $"../../PauseMenu"
 
 func _ready() -> void:
-    head = get_node("head")
-    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	head = get_node("head")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _input(event: InputEvent) -> void:
-    if event is InputEventMouseMotion:
-        rotate_y(-event.relative.x * mouse_sensitivity)
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
 
-        y_rotation = clamp(y_rotation - event.relative.y * mouse_sensitivity, -1.5, 1.0) 
-        head.rotation.x = y_rotation
+		y_rotation = clamp(y_rotation - event.relative.y * mouse_sensitivity, -1.5, 1.0) 
+		head.rotation.x = y_rotation
+		
+
 
 func _physics_process(delta: float) -> void:
-    var direction := Vector3()
+	var direction := Vector3()
 
-    if Input.is_action_pressed("up"):
-        direction.z -= 1
-    if Input.is_action_pressed("down"):
-        direction.z += 1
-    if Input.is_action_pressed("left"):
-        direction.x -= 1
-    if Input.is_action_pressed("right"):
-        direction.x += 1
+	if Input.is_action_pressed("up"):
+		direction.z -= 1
+	if Input.is_action_pressed("down"):
+		direction.z += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("right"):
+		direction.x += 1
 
-    direction = direction.normalized()
+	direction = direction.normalized()
 
-    velocity.x = direction.x * speed
-    velocity.z = direction.z * speed
-    velocity = global_transform.basis * velocity  
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	velocity = global_transform.basis * velocity  
 
-    if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = jump_speed
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
 
-    if not is_on_floor():
-        velocity.y += gravity * delta
-        if velocity.y < max_fall_speed:
-            velocity.y = max_fall_speed
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y < max_fall_speed:
+			velocity.y = max_fall_speed
 
-    move_and_slide()
+	move_and_slide()
 
-    if is_on_floor() and velocity.y < 0:
-        velocity.y = 0
+	if is_on_floor() and velocity.y < 0:
+		velocity.y = 0
