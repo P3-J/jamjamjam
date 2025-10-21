f850d8ea0916c696833687ea5c7a737a0d001943
Jako Keskküla <44132378+P3-J@users.noreply.github.com>
2025-10-21 22:46:54 +0300

    scorefixed (#78)

--- src/scripts/score_screen.gd
+++ src/scripts/score_screen.gd
@@ -10,19 +10,18 @@ var has_submit: bool
 @onready var timer = $Timer2
 
 func _ready() -> void:
-	return
-	#time_text = get_node("timer_text")
-	#list = get_node("ItemList")
-	#text_edit = get_node("TextEdit")
+	time_text = get_node("timer_text")
+	list = get_node("ItemList")
+	text_edit = get_node("TextEdit")
 
-	#get_tree().paused = false
-	#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-	#var minute_str = str(Signalbus.minutes).pad_zeros(2)
-	#var second_str = str(Signalbus.seconds).pad_zeros(2)
-	#var millisecond_str = str(Signalbus.milliseconds).pad_zeros(3)
-	#time_text.text = minute_str + ":" + second_str + ":" + millisecond_str
-	#refresh()
+	var minute_str = str(Signalbus.minutes).pad_zeros(2)
+	var second_str = str(Signalbus.seconds).pad_zeros(2)
+	var millisecond_str = str(Signalbus.milliseconds).pad_zeros(3)
+	time_text.text = minute_str + ":" + second_str + ":" + millisecond_str
+	refresh()
 
 
 
@@ -43,7 +42,9 @@ func int_to_time(total_mils: int):
 
 func submit():
 	print("sending_data")
-	#SilentWolf.Scores.save_score(text_edit.text, -time_to_int(Signalbus.minutes, Signalbus.seconds, Signalbus.milliseconds))
+
+	save_score(text_edit.text, -time_to_int(Signalbus.minutes, Signalbus.seconds, Signalbus.milliseconds))
+	SilentWolf.Scores.save_score(text_edit.text, -time_to_int(Signalbus.minutes, Signalbus.seconds, Signalbus.milliseconds))
 	timer.start()
 
 func refresh():
@@ -83,3 +84,15 @@ func _on_button_pressed() -> void:
 
 func _on_timer_2_timeout() -> void:
 	refresh()
+
+
+
+const SAVE_FILE_PATH := "user://scores.txt"
+func save_score(username: String, time_sec: float) -> void:
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE_READ)
+	if file != null:
+		file.seek_end()
+		file.store_line("%s,%f" % [username, time_sec])
+		file.close()
+	else:
+		push_error("no file boohoo: %s" % SAVE_FILE_PATH)
